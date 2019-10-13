7172d497902115f2b104accec80bd89e9403c491
gabrielpadurean <padurean.gabriel@gmail.com>
2019-10-13 21:44:53 +0300

    Add implementation for the mock controller

--- src/main/java/org/mocker/api/MockController.java
+++ src/main/java/org/mocker/api/MockController.java
@@ -1,8 +1,15 @@
 package org.mocker.api;
 
+import static org.springframework.http.ResponseEntity.notFound;
+import static org.springframework.http.ResponseEntity.status;
+import static org.springframework.util.StringUtils.isEmpty;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.mocker.service.MockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +23,16 @@ public class MockController {
 	private MockService mockService;
 
 	
-	@RequestMapping("/mocks")
-	public ResponseEntity<Object> handleMocks() {
-		return null;
+	@RequestMapping("/mocks/*")
+	public ResponseEntity<String> handleMocks(HttpServletRequest httpServletRequest, @RequestBody(required = false) String requestBody) {
+		String uri = httpServletRequest.getRequestURI();
+		String queryString = httpServletRequest.getQueryString();
+		String endpoint = uri.replace("/v1/api/mocks", "") + (isEmpty(queryString) ? "" : queryString);
+		String method = httpServletRequest.getMethod();
+		
+		return mockService
+				.findMock(endpoint, method, requestBody)
+				.map(mapping -> status(mapping.getResponse().getStatus()).body(mapping.getResponse().getBody()))
+				.orElseGet(() -> notFound().build());
 	}
 }
--- src/main/java/org/mocker/service/MockService.java
+++ src/main/java/org/mocker/service/MockService.java
@@ -1,5 +1,11 @@
 package org.mocker.service;
 
+import static java.util.Optional.ofNullable;
+
+import java.util.Optional;
+
+import org.mocker.domain.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -7,4 +13,18 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MockService {
+	@Autowired
+	private MappingService mappingService;
+	
+	
+	/**
+	 * Finds the mock for the given combination of parameters.
+	 * The body is not used at this moment.
+	 */
+	public Optional<Mapping> findMock(String endpoint, String method, String body) {
+		return ofNullable(mappingService
+				.findByEndpoint(endpoint)
+				.filter(mapping -> mapping.getRequest().getMethod().equalsIgnoreCase(method))
+				.orElse(null));
+	}
 }
