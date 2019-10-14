75169650a0b2fef09e5a186c68c0054060af00fe
gabrielpadurean <padurean.gabriel@gmail.com>
2019-10-14 14:56:38 +0300

    Add endpoint validator

---
+++ src/main/java/org/mocker/validation/mapping/MappingEndpointValidator.java
@@ -0,0 +1,22 @@
+package org.mocker.validation.mapping;
+
+import org.mocker.domain.Mapping;
+import org.mocker.exception.InvalidMethodException;
+import org.mocker.validation.Validator;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author gabrielpadurean
+ */
+@Order(value = 4)
+@Component
+public class MappingEndpointValidator implements Validator<Mapping> {
+
+	@Override
+	public void validate(Mapping mapping) {
+		if (!mapping.getRequest().getEndpoint().startsWith("/")) {
+			throw new InvalidMethodException("The endpoint=" + mapping.getRequest().getEndpoint() + " should start with /");
+		}
+	}
+}
