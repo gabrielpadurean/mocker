d7590dc64445c12e9c7240defc3b8c4c53eb53f1
gabrielpadurean <padurean.gabriel@gmail.com>
2019-12-19 14:18:14 +0200

    Improve page display

--- src/main/resources/public/js/mocker.js
+++ src/main/resources/public/js/mocker.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
 	$('select').formSelect();
+
+	$('.tooltipped').tooltip();
+
+	$("#createMapping").click(function(event) {
+		event.preventDefault();
+
+		var name = $("#name").val();
+		var description = $("#description").val();
+
+		$.ajax({
+			type: 'POST',
+			url: 'https://localhost:8080/v1/api/mappings',
+			data: {
+				name: name,
+				description: description
+			},
+			dataType: 'json'
+		})
+		.done(function(data) {
+			alert("Done: " + data);
+		})
+		.fail(function(data) {
+			alert("Fail: " + data);
+		});
+	});
 });
